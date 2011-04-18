500b5bd931def6a70801436cecb4d75645bcc8e5
Noritaka Horio <holy.shared.design@gmail.com>
2011-04-18 23:44:45 +0900

    add views
    Signed-off-by: Noritaka Horio <holy.shared.design@gmail.com>

--- src/sharedhat-lab/sharedhat.py
+++ src/sharedhat-lab/sharedhat.py
@@ -1,22 +1,34 @@
+import os
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp.util import run_wsgi_app
+from google.appengine.ext.webapp import template
 import artbeat.artbeat
 import foursquare.foursquare
+
 
 from google.appengine.ext import webapp
 
 class MainPage(webapp.RequestHandler):
 
     def get(self):
-        self.response.headers['Content-Type'] = 'text/plain'
-        self.response.out.write('Hello, webapp World!')
+
+#        template_values = {
+#                           'greetings': greetings,
+#                           'url': url,
+#                           'url_linktext': url_linktext,
+#                           }
+
+        self.response.headers['Content-Type'] = 'text/html'
+
+        path = os.path.join(os.path.dirname(__file__), 'views/index.html')
+        self.response.out.write(template.render(path, {}))
+
 
 application = webapp.WSGIApplication([
     ('/', MainPage),
     ('/artbeat', artbeat.artbeat.MainPage),
     ('/foursquare', foursquare.foursquare.MainPage)
-    ],
-    debug=True
+    ], debug=True
 )
 
 def main():
